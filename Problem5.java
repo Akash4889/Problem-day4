751fa1d57af868a3168fad8604a667bf4fb6cfde
Akash4889 <akashsherikar48@gmail.com>
2022-08-17 18:58:53 +0530

    Day 4 Practice problem 5

---
+++ Problem5.java
@@ -0,0 +1,15 @@
+package practiceday4;
+
+public class Problem5 {
+    public static void main(String[] args){
+
+        int a = 5;
+        int b = 10;
+
+        int sum = 0;
+
+        for (int i = a; i <= b; i++)
+            sum = sum + i;
+        System.out.println ("The sum is " + sum);
+    }
+}
