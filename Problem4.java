3ecf8ae65daef7ce5dc07f59e4884a9d6edf1e8f
Akash4889 <akashsherikar48@gmail.com>
2022-08-17 18:58:12 +0530

    Day 4 Practice problem 4

---
+++ Problem4.java
@@ -0,0 +1,15 @@
+package practiceday4;
+
+public class Problem4 {
+    public static void main(String[] args){
+
+       int n=5;
+       int x=0;
+
+        for (x =0; x<n; x++)
+            x=(n*(n+1)/2)-1;
+
+        System.out.println("Sum of the entered natural number is: " + x);
+
+    }
+}
