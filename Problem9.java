71501fb753ea473ba53d4114952db1857252c074
Akash4889 <akashsherikar48@gmail.com>
2022-08-17 19:01:02 +0530

    Day 4 Practice problem 9

---
+++ Problem9.java
@@ -0,0 +1,22 @@
+package practiceday4;
+
+public class Problem9 {
+    public static void main(String[] args){
+
+        int num=9;
+
+        if ( num % 2 == 0)
+
+        System.out.println("The number is not a prime number");
+
+        else if ( num == 2)
+
+            System.out.println("The number is a prime number");
+
+        else
+
+        System.out.println("The number is a prime number");
+
+        // 0 & 1 are not a prime numbers.
+    }
+}
