909449f0fe29f95a6b3cf6006b48f654f03579ec
Akash4889 <akashsherikar48@gmail.com>
2022-08-17 18:57:28 +0530

    Day 4 Practice problem 3

---
+++ Problem3.java
@@ -0,0 +1,18 @@
+package practiceday4;
+
+public class Problem3 {
+
+    public static void main(String[] args) {
+
+   int sum = 0;
+   int n = 10;
+
+
+        for (int x = 1; x <= n; x++)
+            sum += x;
+
+            System.out.println("The sum of first natural number is: " + sum);
+
+
+    }
+}
