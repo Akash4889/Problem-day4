cb783a6487ec6954b9e793e9137edeeeec0e7db3
Akash4889 <akashsherikar48@gmail.com>
2022-08-17 18:54:16 +0530

    Day 4 Practice problem 2

---
+++ Problem2.java
@@ -0,0 +1,15 @@
+package practiceday4;
+
+public class Problem2 {
+    public static void main(String[] args) {
+
+        int number=0;
+
+        if (number%2 == 0)
+
+        System.out.println("The number entered is even");
+
+        else
+            System.out.println("The number entered is odd");
+    }
+}
