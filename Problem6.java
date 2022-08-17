23be338b0c29cf89121455b09364fc9595167d41
Akash4889 <akashsherikar48@gmail.com>
2022-08-17 18:59:24 +0530

    Day 4 Practice problem 6

---
+++ Problem6.java
@@ -0,0 +1,18 @@
+package practiceday4;
+
+public class Problem6 {
+    public static void main(String[] args){
+
+        int a = 20;
+        int b = 40;
+
+        if (a > b)
+
+            System.out.println("Value of a is greater than b" );
+
+        else
+
+        System.out.println("Value of a is lesser than b");
+
+    }
+}
