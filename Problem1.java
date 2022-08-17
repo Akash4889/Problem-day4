a42f95db2addee1a4f636478da213c38242f15c2
Akash4889 <akashsherikar48@gmail.com>
2022-08-17 15:22:52 +0530

    Day 4 Practice problem

---
+++ Problem1.java
@@ -0,0 +1,20 @@
+package practiceday4;
+
+public class Problem1 {
+    public static void main(String[] args){
+
+        int number=9;
+
+
+        if ( number < 0)
+        System.out.println("The number entered is negative: " + number);
+
+        else if  ( number == 0)
+
+        System.out.println("The number is zero: " + number);
+
+        else {
+            System.out.println("The number entered is positive: " + number);
+        }
+    }
+}
