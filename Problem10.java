d1f22b31b33e38c86f0827d1efacd4b2c4fb0b2b
Akash4889 <akashsherikar48@gmail.com>
2022-08-17 19:02:21 +0530

    Day 4 Practice problem 10

---
+++ Problem10.java
@@ -0,0 +1,21 @@
+package practiceday4;
+
+public class Problem10 {
+    public static void main(String[] args){
+
+        int a=10;
+        int b=30;
+        int i=0;
+        int num=0;
+
+        for (i = a; i<=b; i++);
+
+       if (num == i % 2)
+
+        System.out.println("The number is not prime: " + num);
+
+        else;
+
+        System.out.println("The number is prime number: " + num);
+    }
+}
