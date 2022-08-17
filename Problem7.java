a42d506eda1ce3ed5d2735cc47e75d28def572a4
Akash4889 <akashsherikar48@gmail.com>
2022-08-17 18:59:51 +0530

    Day 4 Practice problem 7

---
+++ Problem7.java
@@ -0,0 +1,24 @@
+package practiceday4;
+
+public class Problem7 {
+    public static void main(String[] args){
+
+        int a=9;
+        int b=99;
+        int c=999;
+
+        if (a>b && a>c)
+
+        System.out.println("a is greater than b&c");
+
+        else if (b>a && b>c)
+
+        System.out.println("b is greater than a&c");
+
+        else if  (c>b && c>a)
+
+                    System.out.println("c is greater than a&b");
+
+
+    }
+}
